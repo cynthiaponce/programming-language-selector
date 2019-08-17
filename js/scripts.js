13a8202ea84e251f6b4c25e4dd1f1c9831589d63
Cynthia Ponce <cynthia.ponce21@gmail.com>
2019-08-16 19:49:36 -0700

    Add partial jQuery to the file

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Back End logic
+
+var python = function()
+
+
+
+
+
+//Front End logic
+$(document).ready(function() {
+  $("form#survey").submit(function(event){
+    event.preventDefault();
+    var q1 = $("input#question1").val();
+    var q2 = $("input#question2").val();
+    var q3 = $("input#question3").val();
+    var q4 = $("input#question4").val();
+    var q5 = $("input#question5").val();
+  });
+    var result;
+    
+
+});
